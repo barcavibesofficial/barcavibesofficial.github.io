5028ca4e70cd08096b9158210c6bbb2665faea50
barcavibesofficial <khatamihaneef@gmail.com>
2026-08-02 11:38:59 +0530

    Add La Liga standings fetching functionality
    This script fetches and displays the La Liga standings from the football-data API.

---
+++ laliga.js
@@ -0,0 +1,38 @@
+const API_KEY = "6e65973ffb7e4e4e995cfd15aed8fb1a";
+
+const API_URL = "https://api.football-data.org/v4/competitions/PD/standings";
+
+async function loadLaLigaTable() {
+  try {
+    const response = await fetch(API_URL, {
+      headers: {
+        "X-Auth-Token": API_KEY
+      }
+    });
+
+    const data = await response.json();
+
+    const tableBody = document.getElementById("laliga-body");
+
+    tableBody.innerHTML = "";
+
+    data.standings[0].table.forEach(team => {
+      tableBody.innerHTML += `
+      <tr>
+        <td>${team.position}</td>
+        <td>${team.team.name}</td>
+        <td>${team.playedGames}</td>
+        <td>${team.won}</td>
+        <td>${team.draw}</td>
+        <td>${team.lost}</td>
+        <td>${team.goalDifference}</td>
+        <td>${team.points}</td>
+      </tr>`;
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+loadLaLigaTable();
